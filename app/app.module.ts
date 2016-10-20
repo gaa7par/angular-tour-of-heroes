723c62d156acd056ea2912bd62dda085ac72ec63
gaa7par <gaa7par@gmail.com>
2016-10-20 12:36:06 +0200

    Add DashboardComponent

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import { RouterModule }  from '@angular/router';
 
-import { AppComponent }  from './app.component';
+import { AppComponent }        from './app.component';
+import { DashboardComponent }  from './dashboard.component';
 import { HeroDetailComponent } from './hero-detail.component';
-import { HeroesComponent } from './heroes.component';
-import { HeroService } from './hero.service';
+import { HeroesComponent }     from './heroes.component';
+import { HeroService }         from './hero.service';
 
 @NgModule({
   imports: [
@@ -14,13 +15,23 @@ import { HeroService } from './hero.service';
     FormsModule,
     RouterModule.forRoot([
       {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+      },
+      {
         path: 'heroes',
         component: HeroesComponent
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent
       }
     ])
   ],
   declarations: [
     AppComponent,
+    DashboardComponent,
     HeroDetailComponent,
     HeroesComponent
   ],
